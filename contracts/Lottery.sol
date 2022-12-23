cfce72ae79e0272fc794d8e0106de777ddb1e0ff
amineprogmine <am.assaoui@gmail.com>
2022-12-23 11:06:45 +0100

    update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,22 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 error Lottery__SendEnoughEth();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_Players;
+    event raffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
-    function buyTicket() public payable {
+    function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Lottery__SendEnoughEth();
         }
         s_Players.push(payable(msg.sender));
+        emit raffleEnter(msg.sender);
     }
+
+    function requestRandomWiner() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
